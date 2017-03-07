581019a6611ec7cec54cb13e6721be4c7c1aca04
Sebastian Buck <mail@bastibuck.de>
2017-03-07 17:20:07 +0100

    move files to Contao 3 structure

---
+++ languages/en/default.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package   BackendHelper
+ * @author    Sebastian Buck
+ * @license   LGPL
+ * @copyright Sebastian Buck, 2017
+ */
+
+
+// Quick link to Install-Tool
+$GLOBALS['TL_LANG']['MSC']['quick_installtool']['text'] = "Install-Tool";
+$GLOBALS['TL_LANG']['MSC']['quick_installtool']['title'] = "Open install tool";
