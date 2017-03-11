6eae0dd60b32073dad919024d2f6c79e6e743300
Sebastian Buck <mail@bastibuck.de>
2017-03-11 20:37:04 +0100

    add mssing translation for legend

--- languages/de/tl_user.php
+++ languages/de/tl_user.php
@@ -7,6 +7,8 @@
  * @copyright Sebastian Buck, 2017
  */
 
+// Legends
+$GLOBALS['TL_LANG']['tl_user']['backendhelper_legend'] = 'Backend-Helper Einstellungen';
 
 // Fields
 $GLOBALS['TL_LANG']['tl_user']['useQuickLink'] = array('QuickLink zum Install-Tool', 'Aktivieren Sie den Quick-Link zum Install-Tool in der Header-Bar des Backends.');
--- languages/en/tl_user.php
+++ languages/en/tl_user.php
@@ -7,6 +7,8 @@
  * @copyright Sebastian Buck, 2017
  */
 
+ // Legends
+ $GLOBALS['TL_LANG']['tl_user']['backendhelper_legend'] = 'Backend-Helper Settings';
 
 // Fields
 $GLOBALS['TL_LANG']['tl_user']['useQuickLink'] = array('QuickLink to Install-Tool', 'Activate to add a quick link to the install tool to the backend header.');
